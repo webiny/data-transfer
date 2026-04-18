f3a13b874b3a8b38a7292d5fed11ca5956c9750c
Bruno Zorić <bruno.zoric@gmail.com>
2026-04-18 16:49:38 +0200

    feat: top-level transformers/ barrel

---
+++ src/transformers/index.ts
@@ -0,0 +1,12 @@
+// Factories
+export { createTransformer } from "./createTransformer.ts";
+export { createDdbTransformer } from "./createDdbTransformer.ts";
+export { createOsTransformer } from "./createOsTransformer.ts";
+
+// Built-in transformers grouped by domain
+export * from "./global/index.ts";
+export * from "./cms/index.ts";
+export * from "./file-manager/index.ts";
+export * from "./folders/index.ts";
+export * from "./mailer/index.ts";
+export * from "./security/index.ts";
