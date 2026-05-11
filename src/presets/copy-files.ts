28216f35201b90886a52d03f00017e54dc14482f
Bruno Zorić <bruno.zoric@gmail.com>
2026-05-11 13:50:00 +0200

    fix: copy files preset

--- src/presets/copy-files.ts
+++ src/presets/copy-files.ts
@@ -1,17 +1,21 @@
 import { createTransferPreset } from "~/utils/createTransferPreset.ts";
 import { DdbScanner } from "~/features/DdbScanner/index.ts";
 import { S3Processor } from "~/features/S3Processor/index.ts";
+import { isFmFile } from "~/domain/transform/filters.ts";
+import { createFilter } from "~/domain/pipeline/index.js";
+import { DdbProcessor } from "@/src/index.js";
 
 export default createTransferPreset({
     name: "copy-files",
-    description: "Copy all the S3 files loaded via DynamoDB S3 files.",
+    description: "Copy all the S3 files loaded via DynamoDB regular table - pure copy.",
     configure({ runner, pipelineBuilderFactory: factory }): void {
         const everything = factory
             .create({
                 name: "S3 Files",
                 scanner: DdbScanner,
-                processors: [S3Processor]
+                processors: [DdbProcessor, S3Processor]
             })
+            .filter(createFilter(isFmFile))
             .build();
 
         runner.register(everything);
